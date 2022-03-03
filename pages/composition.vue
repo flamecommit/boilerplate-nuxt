51c83e30f2d51c014d4d1b128a3556e30d9e62f6
신용준 <63823237+shinyj1991@users.noreply.github.com>
2022-03-03 16:39:47 +0900

    .

---
+++ pages/composition.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Composition</p>
+  </div>
+</template>
+
+<script>
+import composition from '@nuxtjs/composition-api'
+
+export default {
+  name: 'CompositionPage',
+  setup(props) {
+    console.log('composition', composition);
+  },
+  created() {
+    console.log('created');
+  },
+  mounted() {
+    console.log('mounted');
+  }
+}
+</script>
